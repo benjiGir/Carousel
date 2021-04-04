2dbaec2a6b22fd01174ef1b5160b24e13204adb4
benji77fr <b.girardpro@gmail.com>
2021-04-04 23:29:11 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const next = document.querySelector(".carousel_next");
+const prev = document.querySelector(".carousel_prev");
+
+
+class Carousel {
+    constructor (visible){
+        this.visible = visible;
+        this.currentItem = 0;
+        this.container = document.querySelector('.carousel_container');
+        this.items = [].slice.call(this.container.children);
+        this.indicators = document.querySelector('.indicators');
+        this.buttons = [];
+        this.setStyle();
+        this.createPagination(this.items.length); 
+    }
+
+    setStyle() {
+        
+        let ratio = this.items.length / this.visible;
+        this.container.style.width = (ratio * 100) + "%";
+        this.items.forEach((item) => item.style.width = ((100 / this.visible) / ratio) + "%");
+    }
+
+    next() {
+        this.goToItem(this.currentItem + 1);
+    }
+
+    previous() {
+        this.goToItem(this.currentItem - 1);
+    }
+
+    goToItem(index) {
+        if (index < 0) {
+            index = this.items.length - this.visible;
+        }
+        else if (index >= this.items.length || ((this.items[this.currentItem + this.visible] === undefined) && index > this.currentItem)) {
+            index = 0;
+        }
+        let translasteX = index * -100 / this.items.length;
+        this.container.style.transform = 'translate3d(' + translasteX + '%, 0, 0)';
+        let activeButton = this.buttons[Math.floor(index / this.visible)];
+        if (activeButton) {
+            this.buttons.forEach(button => button.classList.remove('indicator_active'));
+            activeButton.classList.add('indicator_active');
+        }
+        this.currentItem = index;
+
+    }
+
+    createPagination(numberSlide) {
+        for (let i = 0 ; i < numberSlide ; i++) {
+            let button = document.createElement("div");
+            button.classList.add("indicator");
+            button.addEventListener('click', () => this.goToItem(i));
+            this.indicators.appendChild(button);
+            this.buttons.push(button);
+        };
+        this.buttons[0].classList.add('indicator_active');
+    }
+}
+
+const carousel = new Carousel(1);
+
+
+next.addEventListener("click", (e) => {
+    e.preventDefault()
+    carousel.next()
+});
+
+prev.addEventListener("click", (e) => {
+    e.preventDefault()
+    carousel.previous()
+});
